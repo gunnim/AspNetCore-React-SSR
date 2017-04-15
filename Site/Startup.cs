1944bc1163848a087b87bd7ff6bc1e142cb347f4
Gunnar Már Óttarsson <gunni@starcraft.is>
2017-04-15 20:40:25 +0000

    Configure project for React.NET

--- Site/Startup.cs
+++ Site/Startup.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using React.AspNet;
 
 namespace Site
 {
@@ -19,6 +21,7 @@ namespace Site
                 .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
                 .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
                 .AddEnvironmentVariables();
+
             Configuration = builder.Build();
         }
 
@@ -27,6 +30,9 @@ namespace Site
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+
             // Add framework services.
             services.AddMvc();
         }
@@ -46,6 +52,39 @@ namespace Site
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/Scripts/First.jsx")
+                //  .AddScript("~/Scripts/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/Scripts/bundle.server.js");
+
+                if (env.IsDevelopment())
+                {
+                    config.SetLoadBabel(false)
+                          .SetReuseJavaScriptEngines(false)
+                          //.DisableServerSideRendering()
+                          .AddScriptWithoutTransform("~/scripts/server.js");
+                }
+                else
+                {
+                    config.SetLoadBabel(false)
+                          .AddScriptWithoutTransform("~/scripts/server.min.js");
+                }
+            });
 
             app.UseStaticFiles();
 
